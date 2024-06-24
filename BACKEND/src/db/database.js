e769b92ede1c9e6f3055a0c7fdcdff5bf40bfb48
sr-roboto <marcoslopez123.lu@gmail.com>
2024-06-24 14:55:05 -0300

    agregue contenido demostrativo en la parte de juegos de la home

--- BACKEND/src/db/database.js
+++ BACKEND/src/db/database.js
@@ -1,20 +1,20 @@
 const mysql = require('mysql2/promise');
 
 const conexionDB = async () => {
-   try {
-    const connection = await mysql.createConnection({
-        host: 'localhost',
-        port: 3306,
-        database: 'leximate',
-        user: 'root',
-        password: ''
-    });
-    console.log('Conexion exitosa a la base de datos')
-    return connection;
-   } catch (error) {
-    console.log('Error al conectar', error);
-    throw error;
-   }
+    try {
+        const connection = await mysql.createConnection({
+            host: 'localhost',
+            port: 3306,
+            database: 'leximate',
+            user: 'root',
+            password: ''
+        });
+        console.log('Conexion exitosa a la base de datos')
+        return connection;
+    } catch (error) {
+        console.log('Error al conectar', error);
+        throw error;
+    }
 }
 
 module.exports = {
